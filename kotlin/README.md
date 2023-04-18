dabf21894df9401e10b36e14cebbe59c839242da
mmysliwiec2112 <michal.mysliwiec@student.uj.edu.pl>
2023-04-18 23:04:04 +0200

    kotlin ktor discord bot 4.5 functionalities - done

---
+++ kotlin/README.md
@@ -0,0 +1,10 @@
+Funkcjonalności:
+
+- [x] 3.0 Należy stworzyć aplikację kliencką w Kotlinie we frameworku Ktor,
+która pozwala na przesyłanie wiadomości na platformę Discord
+- [x] 3.5 Aplikacja jest w stanie odbierać wiadomości użytkowników z
+platformy Discord skierowane do aplikacji (bota)
+- [X] 4.0 Zwróci listę kategorii na określone żądanie użytkownika
+- [X] 4.5 Zwróci listę produktów wg żądanej kategorii
+- [ ] 5.0 Aplikacja obsłuży dodatkowo jedną z platform: Slack, Messenfer,
+Webex, Skype
---
+++ kotlin/discord_bot/src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,60 @@
+package com.example
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.example.plugins.*
+import dev.kord.core.Kord
+import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.on
+import dev.kord.gateway.Intent
+import dev.kord.gateway.PrivilegedIntent
+import java.io.File
+
+suspend fun main() {
+    val kord = Kord("MTA5NzUxNTU3MzI5MTc5NDYwMg.G6j-CI.E-czvnWSCWb_4Q5Tt9l_ZEh8_Le52XhO6EbnIU")
+    val categoriesList = listOf<Category>(
+        Category(1, "Science-fiction", listOf("Solaris", "Space Oddysey", "Space Trilogy")),
+        Category(2, "Philosophy", listOf("Republic", "Leviathan", "Either-Or")),
+        Category(3, "Computer Science", listOf("The Art of Computer Programming", "Things a Computer Scientist Rarely Talks About", "C Programming Language"))
+    )
+
+    kord.on<MessageCreateEvent> {
+        if (message.author?.isBot != false) return@on
+        if (message.content == "!help") message.channel.createMessage("commmands:\n" +
+                "!help - prints all of the commands\n" +
+                "!categories - prints the list of the categories\n" +
+                "!category [X] - prints all of the books in specified category"
+        )
+        if (message.content == "!categories")
+        {
+            var printString = ""
+            categoriesList.forEach { category -> printString += "${category.id}Name: ${category.name}\n" }
+            message.channel.createMessage(printString)
+        }
+        if (message.content.startsWith("!category"))
+        {
+            val regex = Regex("!category (.+)")
+            val result = regex.find(message.content)
+
+            if (result != null){
+                val q_name = result.groupValues[1]
+                var category = categoriesList.find { it.name == q_name }
+                message.channel.createMessage("List of items in category $q_name:\n ${category?.books}")
+            }
+            else message.channel.createMessage("Wrong category")
+        }
+    }
+
+    kord.login {
+        // we need to specify this to receive the content of messages
+        @OptIn(PrivilegedIntent::class)
+        intents += Intent.MessageContent
+    }
+}
+
+fun Application.module() {
+    configureRouting()
+}
+
+data class Category(val id: Int, var name: String, val books: List<String>)
